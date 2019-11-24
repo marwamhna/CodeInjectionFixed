d2dac65822314212c844ae02a357e71e1ef68755
marwamhna <marwa.mhna92@gmail.com>
2019-11-24 16:04:40 +0200

    Update code_injection.java

--- code_injection.java
+++ code_injection.java
@@ -7,7 +7,7 @@ public class Example1 {
 			ScriptEngine engine = manager.getEngineByName("JavaScript");
 			System.out.println(args[0]);
 			engine.eval("print('"+ args[0] + "')");	
-engine.eval("print('"+ args[0] + "')");
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
